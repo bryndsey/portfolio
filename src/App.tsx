786ef7dd1e29433bcb5666fd4fab37b8190de21a
Bryan <blinz117@gmail.com>
2023-02-01 14:34:23 -0600

    Add some basic content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Float, RoundedBox } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Euler } from "three";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
+    <div id="App" style={{ height: "100%" }}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {/* <Float speed={10}> */}
+        <RoundedBox args={[1, 2, 0.1]} rotation={new Euler(-0.2, 0.2, 0.1)}>
+          <meshStandardMaterial color="green" />
+        </RoundedBox>
+        {/* </Float> */}
+      </Canvas>
+      {/* <h1 style={{ position: "absolute" }}>Hello, World!</h1> */}
     </div>
   );
 }
