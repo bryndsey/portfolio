de66b043dadc13b98d350181745d25904dd7e550
Bryan Lindsey <blinz117@gmail.com>
2023-01-31 21:23:10 -0600

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    indent: ["warn", 2, { SwitchCase: 1 }],
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
+    "linebreak-style": ["warn", "unix"],
+    "react/react-in-jsx-scope": "off",
+  },
+};
