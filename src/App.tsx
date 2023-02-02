1c7c9b2a141854d7cc4f3c7be107f053ccc8ae8d
Bryan <blinz117@gmail.com>
2023-02-02 09:26:20 -0600

    Show screen on device
    Use Theatric to tweak props in the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,71 @@
-import { Float, RoundedBox } from "@react-three/drei";
+import { Html, PerspectiveCamera, RoundedBox } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useControls, types } from "theatric";
 import { Euler } from "three";
 
+const Device = () => {
+  const { rotation, size, bezelSize } = useControls(
+    {
+      rotation: {
+        x: types.number(-0.2, { range: [-Math.PI, Math.PI] }),
+        y: types.number(0.2, { range: [-Math.PI, Math.PI] }),
+        z: types.number(0.1, { range: [-Math.PI, Math.PI] }),
+      },
+      size: { width: 1, height: 2, thickness: 0.1 },
+      bezelSize: 32,
+    },
+    { folder: "device" }
+  );
+
+  return (
+    <RoundedBox
+      args={[size.width, size.height, size.thickness]}
+      rotation={new Euler(rotation.x, rotation.y, rotation.z)}
+      radius={0.033}
+    >
+      <meshStandardMaterial color="dimgray" />
+      <Html
+        transform
+        occlude
+        distanceFactor={1}
+        position={[0, 0.01, size.thickness / 2 + 0.001]}
+        style={{
+          background: "white",
+          margin: 0,
+          padding: 0,
+          borderRadius: "8px",
+          width: 400 * size.width - bezelSize,
+          height: 400 * size.height - bezelSize,
+        }}
+      >
+        <div style={{ padding: 8 }}>
+          <p>Hello, World!</p>
+          <button>Click me!</button>
+        </div>
+      </Html>
+    </RoundedBox>
+  );
+};
+
 function App() {
+  const { position } = useControls(
+    {
+      position: { x: 0, y: 0, z: 5 },
+    },
+    { folder: "camera" }
+  );
+
   return (
     <div id="App" style={{ height: "100%" }}>
       <Canvas>
+        <PerspectiveCamera
+          makeDefault
+          position={[position.x, position.y, position.z]}
+        />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        {/* <Float speed={10}> */}
-        <RoundedBox args={[1, 2, 0.1]} rotation={new Euler(-0.2, 0.2, 0.1)}>
-          <meshStandardMaterial color="green" />
-        </RoundedBox>
-        {/* </Float> */}
+        <Device />
       </Canvas>
-      {/* <h1 style={{ position: "absolute" }}>Hello, World!</h1> */}
     </div>
   );
 }
