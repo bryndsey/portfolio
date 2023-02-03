73e3bdd11321b450ed146a4a1ad3bfb631458919
Bryan <blinz117@gmail.com>
2023-02-03 14:18:18 -0600

    Start adding fake icons to device

--- src/ScreenContent.tsx
+++ src/ScreenContent.tsx
@@ -1,3 +1,23 @@
+interface DeviceAppIconProps {
+  appName: string;
+  color: string;
+}
+
+const DeviceAppIcon = (props: DeviceAppIconProps) => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: props.color,
+          borderRadius: 8,
+          aspectRatio: "1 / 1",
+        }}
+      />
+      <p style={{ textAlign: "center" }}>{props.appName}</p>
+    </div>
+  );
+};
+
 export function ScreenContent() {
   return (
     <div style={{ padding: 8 }}>
@@ -9,6 +29,21 @@ export function ScreenContent() {
         <li>Music</li>
         <li>Video Games</li>
       </ul>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          // gridAutoRows: 48,
+          gap: 16,
+          padding: 8,
+        }}
+      >
+        <DeviceAppIcon appName="Test 1" color="green" />
+        <DeviceAppIcon appName="Test 2" color="blue" />
+        <DeviceAppIcon appName="Test 3" color="red" />
+        <DeviceAppIcon appName="Test 4" color="tan" />
+        <DeviceAppIcon appName="Test 5" color="grey" />
+      </div>
     </div>
   );
 }
