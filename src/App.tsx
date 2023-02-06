105e3908b3bc4bc2a956b90dcaa5b1bd52c0eabc
Bryan <blinz117@gmail.com>
2023-02-06 05:06:36 -0600

    Use container to position guitar

--- src/App.tsx
+++ src/App.tsx
@@ -73,6 +73,32 @@ const DeviceContainer = () => {
   );
 };
 
+const GuitarContainer = () => {
+  const { position, rotation } = useControls(
+    {
+      position: {
+        x: types.number(0.2, { nudgeMultiplier: 0.1 }),
+        y: types.number(0, { nudgeMultiplier: 0.1 }),
+        z: types.number(0, { nudgeMultiplier: 0.1 }),
+      },
+      rotation: {
+        x: types.number(Math.PI / 2, { range: [-Math.PI, Math.PI] }),
+        y: types.number(-0.25, { range: [-Math.PI, Math.PI] }),
+        z: types.number(0.6, { range: [-Math.PI, Math.PI] }),
+      },
+    },
+    { folder: "guitar" }
+  );
+  return (
+    <group
+      position={[position.x, position.y, position.z]}
+      rotation={[rotation.x, rotation.y, rotation.z]}
+    >
+      <Guitar />
+    </group>
+  );
+};
+
 const backgroundColor = "mediumseagreen";
 
 function App() {
@@ -95,7 +121,7 @@ function App() {
         <ambientLight intensity={0.5} color={backgroundColor} />
         <directionalLight position={[10, 10, 10]} intensity={1} />
         <DeviceContainer />
-        <Guitar />
+        <GuitarContainer />
       </Canvas>
     </div>
   );
