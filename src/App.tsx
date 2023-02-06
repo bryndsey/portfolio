be3575971f7af56b073b7afd045ba8a9c25e605f
Bryan <blinz117@gmail.com>
2023-02-06 03:53:42 -0600

    Move device container component to app
    Since the App rather than the device itself will manage position, rotation, etc. it makes more sense to have it there, or at least not contained in the device file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,77 @@
 import { PerspectiveCamera, Stats } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useControls } from "theatric";
+import { types, useControls } from "theatric";
 import { Device } from "./Device";
+import { Guitar } from "./Guitar";
+
+const deviceStartPosition = { x: -0.5, y: -0.5, z: 0 };
+const deviceStartRotation = { x: -0.5 * Math.PI, y: 0, z: 0.66 };
+const deviceSize = { width: 0.15, height: 0.3, thickness: 0.02 };
+const deviceBezelSize = 64;
+
+const DeviceContainer = () => {
+  const { position, rotation, size, bezelSize } = useControls(
+    {
+      position: {
+        x: types.number(deviceStartPosition.x, { nudgeMultiplier: 0.1 }),
+        y: types.number(deviceStartPosition.y, { nudgeMultiplier: 0.1 }),
+        z: types.number(deviceStartPosition.z, { nudgeMultiplier: 0.1 }),
+      },
+      rotation: {
+        x: types.number(deviceStartRotation.x, { range: [-Math.PI, Math.PI] }),
+        y: types.number(deviceStartRotation.y, { range: [-Math.PI, Math.PI] }),
+        z: types.number(deviceStartRotation.z, { range: [-Math.PI, Math.PI] }),
+      },
+      size: {
+        width: types.number(deviceSize.width, { nudgeMultiplier: 0.1 }),
+        height: types.number(deviceSize.height, { nudgeMultiplier: 0.1 }),
+        thickness: types.number(deviceSize.thickness, {
+          nudgeMultiplier: 0.01,
+        }),
+      },
+      bezelSize: deviceBezelSize,
+    },
+    { folder: "device" }
+  );
+
+  // const rotation = startRotation;
+  // const size = deviceSize;
+  // const bezelSize = deviceBezelSize;
+
+  // const groupRef = useRef<Group>(null);
+
+  // const scrollData = useScroll();
+  // useFrame(() => {
+  //   const deviceGroup = groupRef.current;
+  //   if (deviceGroup === null) return;
+  //   const scrollRange = scrollData.range(0, 2 / 3);
+  //   const currentRotation = {
+  //     x: MathUtils.lerp(rotation.x, 0, scrollRange),
+  //     y: MathUtils.lerp(rotation.y, 0, scrollRange),
+  //     z: MathUtils.lerp(rotation.z, 0, scrollRange),
+  //   };
+  //   deviceGroup.setRotationFromEuler(
+  //     new Euler(currentRotation.x, currentRotation.y, currentRotation.z)
+  //   );
+  //   deviceGroup.position.setX(MathUtils.lerp(position.x, 0, scrollRange));
+  //   deviceGroup.position.setY(MathUtils.lerp(position.y, 0, scrollRange));
+  //   deviceGroup.position.setZ(MathUtils.lerp(position.z, 0.5, scrollRange));
+  // });
+
+  return (
+    <group
+      // ref={groupRef}
+      position={[position.x, position.y, position.z]}
+      rotation={[rotation.x, rotation.y, rotation.z]}
+      onClick={() => {
+        console.log("Clicked group");
+      }}
+      onPointerMissed={() => console.log("Clicked outside group")}
+    >
+      <Device {...size} bezelSize={bezelSize} />
+    </group>
+  );
+};
 
 const backgroundColor = "mediumseagreen";
 
@@ -23,7 +93,8 @@ function App() {
         />
         <ambientLight intensity={0.5} color={backgroundColor} />
         <directionalLight position={[10, 10, 10]} intensity={1} />
-        <Device />
+        <DeviceContainer />
+        <Guitar />
       </Canvas>
     </div>
   );
--- src/Device.tsx
+++ src/Device.tsx
@@ -1,8 +1,6 @@
 import { Html, RoundedBox } from "@react-three/drei";
 import { Vector3 } from "@react-three/fiber";
-import { useRef } from "react";
-import { types, useControls } from "theatric";
-import { Color, Group } from "three";
+import { Color } from "three";
 import { ScreenContent } from "./ScreenContent";
 
 interface DeviceScreenProps {
@@ -44,7 +42,7 @@ interface DeviceObjectProps {
   bezelSize: number;
 }
 
-function DeviceObject(props: DeviceObjectProps) {
+export function Device(props: DeviceObjectProps) {
   return (
     <RoundedBox
       onClick={() => {
@@ -68,72 +66,3 @@ function DeviceObject(props: DeviceObjectProps) {
     </RoundedBox>
   );
 }
-
-const startPosition = { x: -0.5, y: -0.5, z: 0 };
-const startRotation = { x: -0.5 * Math.PI, y: 0, z: 0.66 };
-const deviceSize = { width: 0.15, height: 0.3, thickness: 0.02 };
-const deviceBezelSize = 64;
-
-export const Device = () => {
-  const { position, rotation, size, bezelSize } = useControls(
-    {
-      position: {
-        x: types.number(startPosition.x, { nudgeMultiplier: 0.1 }),
-        y: types.number(startPosition.y, { nudgeMultiplier: 0.1 }),
-        z: types.number(startPosition.z, { nudgeMultiplier: 0.1 }),
-      },
-      rotation: {
-        x: types.number(startRotation.x, { range: [-Math.PI, Math.PI] }),
-        y: types.number(startRotation.y, { range: [-Math.PI, Math.PI] }),
-        z: types.number(startRotation.z, { range: [-Math.PI, Math.PI] }),
-      },
-      size: {
-        width: types.number(deviceSize.width, { nudgeMultiplier: 0.1 }),
-        height: types.number(deviceSize.height, { nudgeMultiplier: 0.1 }),
-        thickness: types.number(deviceSize.thickness, {
-          nudgeMultiplier: 0.01,
-        }),
-      },
-      bezelSize: deviceBezelSize,
-    },
-    { folder: "device" }
-  );
-
-  // const rotation = startRotation;
-  // const size = deviceSize;
-  // const bezelSize = deviceBezelSize;
-
-  const groupRef = useRef<Group>(null);
-
-  // const scrollData = useScroll();
-  // useFrame(() => {
-  //   const deviceGroup = groupRef.current;
-  //   if (deviceGroup === null) return;
-  //   const scrollRange = scrollData.range(0, 2 / 3);
-  //   const currentRotation = {
-  //     x: MathUtils.lerp(rotation.x, 0, scrollRange),
-  //     y: MathUtils.lerp(rotation.y, 0, scrollRange),
-  //     z: MathUtils.lerp(rotation.z, 0, scrollRange),
-  //   };
-  //   deviceGroup.setRotationFromEuler(
-  //     new Euler(currentRotation.x, currentRotation.y, currentRotation.z)
-  //   );
-  //   deviceGroup.position.setX(MathUtils.lerp(position.x, 0, scrollRange));
-  //   deviceGroup.position.setY(MathUtils.lerp(position.y, 0, scrollRange));
-  //   deviceGroup.position.setZ(MathUtils.lerp(position.z, 0.5, scrollRange));
-  // });
-
-  return (
-    <group
-      ref={groupRef}
-      position={[position.x, position.y, position.z]}
-      rotation={[rotation.x, rotation.y, rotation.z]}
-      onClick={() => {
-        console.log("Clicked group");
-      }}
-      onPointerMissed={() => console.log("Clicked outside group")}
-    >
-      <DeviceObject {...size} bezelSize={bezelSize} />
-    </group>
-  );
-};
