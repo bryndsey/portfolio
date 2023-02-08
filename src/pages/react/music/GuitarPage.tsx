16e634d65095620b67d8c8f7e0a741eeea250061
Bryan <blinz117@gmail.com>
2023-02-07 20:30:52 -0600

    Clean up guitar page

--- src/pages/react/music/GuitarPage.tsx
+++ src/pages/react/music/GuitarPage.tsx
@@ -1,9 +1,8 @@
 import { useRef } from "react";
-import { types, useControls } from "theatric";
 import { Group } from "three";
-import { Guitar } from "./Guitar";
 import { PageComponentProps } from "../../Pages";
 import { useScrollPages } from "../../useScrollPages";
+import { Guitar } from "./Guitar";
 
 export const GuitarPage = (props: PageComponentProps) => {
   const groupRef = useRef<Group>(null);
@@ -18,38 +17,18 @@ export const GuitarPage = (props: PageComponentProps) => {
 
       const viewportHeight = state.viewport.height;
       groupRef.current.position.setY(yPercent * viewportHeight);
+      groupRef.current.position.setX(state.viewport.width / 5);
     }
   );
 
   return (
     <group ref={groupRef}>
-      <GuitarContainer />
-    </group>
-  );
-};
-
-const GuitarContainer = () => {
-  const { position, rotation } = useControls(
-    {
-      position: {
-        x: types.number(0.2, { nudgeMultiplier: 0.1 }),
-        y: types.number(0, { nudgeMultiplier: 0.1 }),
-        z: types.number(0, { nudgeMultiplier: 0.1 }),
-      },
-      rotation: {
-        x: types.number(Math.PI / 2, { range: [-Math.PI, Math.PI] }),
-        y: types.number(-0.25, { range: [-Math.PI, Math.PI] }),
-        z: types.number(0.6, { range: [-Math.PI, Math.PI] }),
-      },
-    },
-    { folder: "guitar" }
-  );
-  return (
-    <group
-      position={[position.x, position.y, position.z]}
-      rotation={[rotation.x, rotation.y, rotation.z]}
-    >
-      <Guitar />
+      <group
+        // position={[position.x, position.y, position.z]}
+        rotation={[Math.PI / 2, -0.25, 0.6]}
+      >
+        <Guitar />
+      </group>
     </group>
   );
 };
