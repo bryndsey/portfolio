6c020532c34af2c273f79e24158676968738cbde
Bryan <blinz117@gmail.com>
2023-02-08 16:19:04 -0600

    Properly integrate Intro text with scroll

--- src/pages/IntroText.tsx
+++ src/pages/IntroText.tsx
@@ -2,13 +2,30 @@ import { Html } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useRef } from "react";
 import { Group } from "three";
+import { useHtmlPortal } from "../useHtmlPortal";
 import { PageComponentProps } from "./Pages";
+import { useScrollPages } from "./useScrollPages";
 
 // TODO: Actually use the props...
 export const IntroText = (props: PageComponentProps) => {
   const size = useThree((state) => state.size);
 
+  const htmlPortal = useHtmlPortal();
+
   const groupRef = useRef<Group>(null);
+
+  useScrollPages(
+    props.startPageIndex,
+    props.exitPageIndex,
+    (enterAmount, exitAmount, state, delta) => {
+      if (groupRef.current === null) return;
+
+      const yPercent = enterAmount + exitAmount;
+
+      const viewportHeight = state.viewport.height;
+      groupRef.current.position.setY(yPercent * viewportHeight);
+    }
+  );
 
   useFrame((state) => {
     const mouse = state.mouse;
@@ -27,6 +44,7 @@ export const IntroText = (props: PageComponentProps) => {
           width: size.width * 0.66,
           fontFamily: "sans-serif",
         }}
+        portal={{ current: htmlPortal }}
       >
         <div
           style={{
