7e5341f1646457fc810e17833542c4be7b4313dd
Bryan <blinz117@gmail.com>
2023-02-08 21:31:31 -0600

    Clean up imports

--- src/pages/react/music/GuitarPage.tsx
+++ src/pages/react/music/GuitarPage.tsx
@@ -1,13 +1,8 @@
-import { Html, Text } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { useRef } from "react";
 import { Group } from "three";
-import {
-  AndroidTag,
-  ProjectDescription,
-  ReactTag,
-  UnityTag,
-} from "../../../ProjectDescription";
+import { ProjectDescription, ReactTag } from "../../../ProjectDescription";
 import { useHtmlPortal } from "../../../useHtmlPortal";
 import { PageComponentProps } from "../../Pages";
 import { useScrollPages } from "../../useScrollPages";
