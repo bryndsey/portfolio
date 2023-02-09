e574c833ef09d253b04109a56b1699df0e303609
Bryan <blinz117@gmail.com>
2023-02-09 09:04:15 -0600

    Remove unneeded stuff

--- src/pages/android/DevicePage.tsx
+++ src/pages/android/DevicePage.tsx
@@ -1,32 +1,15 @@
 import { useRef } from "react";
-import { types, useControls } from "theatric";
 import { Euler, Group, MathUtils } from "three";
-import { Device } from "./Device";
 import { PageComponentProps } from "../Pages";
 import { useScrollPages } from "../useScrollPages";
+import { Device } from "./Device";
 
-// const deviceStartPosition = { x: -0.5, y: 0, z: 0 };
-// const deviceStartRotation = { x: -0.5 * Math.PI, y: 0, z: 0.66 };
 const deviceSize = { width: 0.15, height: 0.3, thickness: 0.02 };
 const deviceBezelSize = 64;
 
 export const DevicePage = (props: PageComponentProps) => {
   const groupRef = useRef<Group>(null);
   const innerGroupRef = useRef<Group>(null);
-
-  // const { size, bezelSize } = useControls(
-  //   {
-  //     size: {
-  //       width: types.number(deviceSize.width, { nudgeMultiplier: 0.1 }),
-  //       height: types.number(deviceSize.height, { nudgeMultiplier: 0.1 }),
-  //       thickness: types.number(deviceSize.thickness, {
-  //         nudgeMultiplier: 0.01,
-  //       }),
-  //     },
-  //     bezelSize: deviceBezelSize,
-  //   },
-  //   { folder: "device" }
-  // );
 
   useScrollPages(
     props.startPageIndex,
