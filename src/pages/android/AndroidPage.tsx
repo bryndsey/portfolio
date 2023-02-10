577b99323ecc5a6b7c0954d393d3cc0fa2d09873
Bryan <blinz117@gmail.com>
2023-02-10 07:08:22 -0600

    Show prompt when no app is selected
    Also changes how text is hidden/shown

--- src/pages/android/AndroidPage.tsx
+++ src/pages/android/AndroidPage.tsx
@@ -2,7 +2,7 @@ import { animated, useTransition } from "@react-spring/web";
 import { Html } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { atom, useAtom } from "jotai";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Euler, Group, MathUtils } from "three";
 import {
   AndroidTag,
@@ -73,15 +73,21 @@ export const AndroidPage = (props: PageComponentProps) => {
   const groupRef = useRef<Group>(null);
   const innerGroupRef = useRef<Group>(null);
 
-  const [selectedApp, selectApp] = useSelectedAndroidApp();
-
-  const transitions = useTransition(selectedApp, {
-    key: selectedApp,
+  const [selectedApp] = useSelectedAndroidApp();
+  const selectedAppTransitions = useTransition(selectedApp, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
     config: { duration: 300 },
     exitBeforeEnter: true,
+  });
+
+  const [showText, setShowText] = useState(false);
+  const showTextTransitions = useTransition(showText, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: 300 },
   });
 
   const htmlPortal = useHtmlPortal();
@@ -97,8 +103,9 @@ export const AndroidPage = (props: PageComponentProps) => {
 
       const progress = enterAmount + exitAmount;
 
-      if (selectedApp !== null && progress !== 0) {
-        selectApp(null);
+      const shouldShowText = progress === 0;
+      if (shouldShowText !== showText) {
+        setShowText(shouldShowText);
       }
 
       const position = MathUtils.lerp(
@@ -140,17 +147,28 @@ export const AndroidPage = (props: PageComponentProps) => {
           portal={{ current: htmlPortal }}
           distanceFactor={1}
         >
-          {transitions((style, app) => (
-            <animated.div style={style}>
-              {app && (
-                <ProjectDescription
-                  projectName={app.name}
-                  descriptionText={app.description}
-                  tags={app.projectTags}
-                />
-              )}
-            </animated.div>
-          ))}
+          {showTextTransitions(
+            (showStyle, show) =>
+              show && (
+                <animated.div style={showStyle}>
+                  {selectedAppTransitions((style, app) => (
+                    <animated.div style={style}>
+                      {app ? (
+                        <ProjectDescription
+                          projectName={app.name}
+                          descriptionText={app.description}
+                          tags={app.projectTags}
+                        />
+                      ) : (
+                        <div className="text-center text-2xl">
+                          Select an app to learn more
+                        </div>
+                      )}
+                    </animated.div>
+                  ))}
+                </animated.div>
+              )
+          )}
         </Html>
       </group>
     </group>
