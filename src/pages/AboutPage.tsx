92d860edb6f8146a5d662e2f1b5565256ef910ab
Bryan <blinz117@gmail.com>
2023-03-18 22:17:43 -0500

    Start building 3d list of things I make

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,22 +1,96 @@
-import { Html } from "@react-three/drei";
+import { Billboard, Float, Html, Text } from "@react-three/drei";
+import { RootState, useFrame } from "@react-three/fiber";
 import { Suspense, useRef } from "react";
-import { Group } from "three";
+import { Group, MathUtils, Vector3 } from "three";
 import { useHtmlPortal } from "../useHtmlPortal";
 import { PageComponentProps } from "./Pages";
 import { useScrollPages } from "./useScrollPages";
 
-const listOfThingsIMake = [
-  "React apps",
-  "Music",
-  "Games",
-  "3D models",
-  "Android apps",
+interface ThingIMake {
+  name: string;
+  positionFn: (state: RootState) => number[];
+}
+
+const newListOfThingsIMake: ThingIMake[] = [
+  {
+    name: "React apps",
+    positionFn: (state) => [
+      state.viewport.width * 0.1,
+      state.viewport.height * 0.1,
+      0.3,
+    ],
+  },
+  {
+    name: "Music",
+    positionFn: (state) => [
+      state.viewport.width * -0.25,
+      state.viewport.height * 0.2,
+      0.1,
+    ],
+  },
+  {
+    name: "3D models",
+    positionFn: (state) => [
+      state.viewport.width * 0.3,
+      state.viewport.height * -0.175,
+      -0.1,
+    ],
+  },
+  {
+    name: "Games",
+    positionFn: (state) => [0, state.viewport.height * -0.3, 0.05],
+  },
+  {
+    name: "Android apps",
+    positionFn: (state) => [
+      state.viewport.width * -0.25,
+      state.viewport.height * -0.1,
+      -0.35,
+    ],
+  },
 ];
+
+const FloatingThing = (props: { thing: ThingIMake }) => {
+  const { thing } = props;
+
+  const htmlPortal = useHtmlPortal();
+  const groupRef = useRef<Group>(null!);
+  const htmlRef = useRef<HTMLDivElement>(null!);
+
+  useFrame((state) => {
+    const position = thing.positionFn(state);
+    groupRef.current.position.set(position[0], position[1], position[2]);
+
+    htmlRef.current.style.opacity = `${MathUtils.mapLinear(
+      position[2],
+      -3,
+      1,
+      0,
+      1
+    )}`;
+  });
+
+  return (
+    <group ref={groupRef}>
+      <Html
+        ref={htmlRef}
+        transform
+        portal={{ current: htmlPortal }}
+        distanceFactor={1}
+      >
+        <p className="text-2xl sm:text-4xl md:text-6xl font-bold whitespace-nowrap">
+          {thing.name}
+        </p>
+      </Html>
+    </group>
+  );
+};
 
 export const AboutPage = (props: PageComponentProps) => {
   const htmlPortal = useHtmlPortal();
   const groupRef = useRef<Group>(null);
   const contentRef = useRef<HTMLDivElement>(null);
+  const wordCloudGroupRef = useRef<Group>(null!);
 
   useScrollPages(
     props.startPageIndex,
@@ -40,15 +114,20 @@ export const AboutPage = (props: PageComponentProps) => {
     <Suspense fallback={null}>
       <group ref={groupRef}>
         <Html ref={contentRef} fullscreen portal={{ current: htmlPortal }}>
-          <div className="h-full w-3/4 flex flex-col justify-around m-auto">
-            <p className="font-semibold text-2xl sm:text-4xl md:text-6xl">
-              {`I like to make things. ${listOfThingsIMake.join(". ")}.`}
+          <div className="h-full w-full flex flex-col justify-between m-auto px-4 py-8">
+            <p className="font-semibold text-5xl md:text-7xl font-handwritten text-center">
+              I like to make things.
             </p>
-            <p className="text-base sm:text-xl md:text-3xl text-center font-handwritten">
+            <p className="text-xl md:text-4xl text-center font-handwritten">
               {"Keep scrolling to see some things I've made."}
             </p>
           </div>
         </Html>
+        <group ref={wordCloudGroupRef}>
+          {newListOfThingsIMake.map((thing) => (
+            <FloatingThing key={thing.name} thing={thing} />
+          ))}
+        </group>
       </group>
     </Suspense>
   );
