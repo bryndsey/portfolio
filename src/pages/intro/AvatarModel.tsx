52d4d7f0cd66628408f64c34517e4ca8ca4a9e35
Bryan <blinz117@gmail.com>
2023-05-22 16:34:11 -0500

    Start animating head to follow mouse cursor

--- src/pages/intro/AvatarModel.tsx
+++ src/pages/intro/AvatarModel.tsx
@@ -1,12 +1,91 @@
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import Avatar from "./avatar.glb?url";
+import { GLTF } from "three-stdlib";
+import { Group, Vector3 } from "three";
+import { useFrame } from "@react-three/fiber";
 
-export function AvatarModel() {
-  const gltf = useGLTF(Avatar);
+type GLTFResult = GLTF & {
+  nodes: {
+    Eyebrows: THREE.Mesh;
+    Eyes: THREE.Mesh;
+    Nose: THREE.Mesh;
+    Hair_Parted: THREE.Mesh;
+    Head: THREE.Mesh;
+    PantsLegs: THREE.Mesh;
+    Shoes_1: THREE.Mesh;
+    Shoes_2: THREE.Mesh;
+    PantsTop: THREE.Mesh;
+    Body: THREE.Mesh;
+    Sleeves_Down: THREE.Mesh;
+    Arms_Down: THREE.Mesh;
+  };
+  materials: {
+    hair: THREE.MeshStandardMaterial;
+    eye: THREE.MeshPhysicalMaterial;
+    nose: THREE.MeshStandardMaterial;
+    skin: THREE.MeshStandardMaterial;
+    Pants: THREE.MeshStandardMaterial;
+    Shoes_main: THREE.MeshStandardMaterial;
+    Shoes_bottom: THREE.MeshStandardMaterial;
+    Shirt: THREE.MeshStandardMaterial;
+  };
+};
+
+const vector = new Vector3();
+export function AvatarModel(props: JSX.IntrinsicElements["group"]) {
+  const { nodes, materials } = useGLTF(Avatar) as GLTFResult;
+  const headGroup = useRef<Group | null>(null);
+
+  useFrame((state) => {
+    if (headGroup.current === null) return;
+
+    const offset = headGroup.current.getWorldPosition(vector);
+    headGroup.current.lookAt(
+      state.pointer.x - offset.x,
+      state.pointer.y,
+      state.camera.position.z
+    );
+  });
+
   return (
     <Suspense fallback={null}>
-      <primitive object={gltf.scene} />
+      <group {...props} dispose={null}>
+        <group position={[0, 0.38, 0]} ref={headGroup}>
+          <mesh
+            geometry={nodes.Eyebrows.geometry}
+            material={materials.hair}
+            rotation={[Math.PI / 2, 0, 0]}
+          />
+          <mesh
+            geometry={nodes.Eyes.geometry}
+            material={materials.eye}
+            position={[0, 0.13, 0]}
+          />
+          <mesh geometry={nodes.Nose.geometry} material={materials.nose} />
+          <mesh
+            geometry={nodes.Hair_Parted.geometry}
+            material={materials.hair}
+          />
+          <mesh geometry={nodes.Head.geometry} material={materials.skin} />
+        </group>
+        <mesh geometry={nodes.PantsLegs.geometry} material={materials.Pants} />
+        <mesh
+          geometry={nodes.Shoes_1.geometry}
+          material={materials.Shoes_main}
+        />
+        <mesh
+          geometry={nodes.Shoes_2.geometry}
+          material={materials.Shoes_bottom}
+        />
+        <mesh geometry={nodes.PantsTop.geometry} material={materials.Pants} />
+        <mesh geometry={nodes.Body.geometry} material={materials.Shirt} />
+        <mesh
+          geometry={nodes.Sleeves_Down.geometry}
+          material={materials.Shirt}
+        />
+        <mesh geometry={nodes.Arms_Down.geometry} material={materials.skin} />
+      </group>
     </Suspense>
   );
 }
