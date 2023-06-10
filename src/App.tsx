cd48466ba9ab8d867b2aba05f65bb1200063b089
Bryan <blinz117@gmail.com>
2023-06-10 10:28:36 -0500

    Start adding a blob made with a shader

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import {
   Html,
   OrbitControls,
   PerspectiveCamera,
+  Plane,
   Preload,
   ScrollControls,
   Stats,
+  shaderMaterial,
   useHelper,
 } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, extend, useFrame } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
-import { Suspense, useRef } from "react";
+import { Suspense, useEffect, useRef } from "react";
 // import { useControls } from "theatric";
-import { CameraHelper, MathUtils, Vector2, Vector3 } from "three";
+import { CameraHelper, Color, MathUtils, Vector2, Vector3 } from "three";
 import HDRI from "./assets/empty_warehouse_01_1k.hdr?url";
 import { pages } from "./pages/Pages";
 import { ReactLenis } from "@studio-freight/react-lenis";
@@ -89,6 +91,58 @@ function LoadingIndicator() {
   );
 }
 
+const BlobShaderMaterial = shaderMaterial(
+  { uTime: 0, uBlobbiness: 1, color: new Color("green") },
+  `
+  varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  `
+  uniform float time;
+    uniform float uTime;
+    uniform float uBlobbiness;
+    uniform vec3 color;
+    varying vec2 vUv;
+    void main() {
+      vec2 wavedUv = vec2(
+        vUv.x + sin(vUv.y * 2.9 + uTime) * 0.05 * uBlobbiness,
+        vUv.y + sin(vUv.x * 5.1 + uTime) * 0.05 * uBlobbiness
+      );
+      float strength = step(0.4, distance(wavedUv, vec2(0.5)));
+      gl_FragColor.rgba = vec4(color.xyz, (1.0 - strength) * 0.33);
+    }
+  `
+);
+
+extend({ BlobShaderMaterial });
+
+interface BlobProps {
+  speed?: number;
+  blobbiness?: number;
+}
+
+function Blob(props: BlobProps) {
+  const { speed = 1, blobbiness = 0.5 } = props;
+  const blobMaterialRef = useRef<BlobShaderMaterial>(null!);
+
+  useEffect(() => {
+    blobMaterialRef.current.uBlobbiness = blobbiness;
+  }, [blobbiness]);
+
+  useFrame((state, delta) => {
+    blobMaterialRef.current.uTime += delta * speed;
+  });
+
+  return (
+    <Plane>
+      <blobShaderMaterial ref={blobMaterialRef} />
+    </Plane>
+  );
+}
+
 function App() {
   // const { showStats } = useControls({
   //   showStats: true,
@@ -136,6 +190,8 @@ function App() {
           <Suspense fallback={<LoadingIndicator />}>
             <Environment files={HDRI} />
             <Preload all />
+
+            <Blob speed={0.3} blobbiness={2} />
 
             {/* <ScrollControls pages={pages.totalPages}> */}
             <ambientLight intensity={0.15} />
