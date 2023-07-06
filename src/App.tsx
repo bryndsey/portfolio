fef168724c79004805ca08ddd1df2b4b5467db4c
Bryan <blinz117@gmail.com>
2023-07-06 16:31:11 -0500

    Animate blob entrances

--- src/App.tsx
+++ src/App.tsx
@@ -107,41 +107,59 @@ function LoadingIndicator() {
 function BackgroundBlobs() {
   const { loadingTransistionValue } = useIsLoaded();
   return (
-    <animated.group scale={loadingTransistionValue}>
-      <Float floatIntensity={0.5} speed={0.66} rotationIntensity={0.5}>
-        <group position={[2.66, 1, -5.5]}>
-          <Blob
-            speed={0.2}
-            blobbiness={1}
-            size={1.5}
-            color={"limegreen"}
-            opacity={0.15}
-          />
-        </group>
-      </Float>
-      <Float floatIntensity={0.5} speed={0.66} rotationIntensity={0.5}>
-        <group position={[-2.5, 2.5, -6]}>
-          <Blob
-            speed={0.2}
-            blobbiness={1.2}
-            size={3}
-            color={"limegreen"}
-            opacity={0.15}
-          />
-        </group>
-      </Float>
-      <Float floatIntensity={0.5} speed={0.5} rotationIntensity={0.5}>
-        <group position={[-0.5, -3.5, -5]}>
-          <Blob
-            speed={0.2}
-            blobbiness={1.1}
-            size={6.5}
-            color={"limegreen"}
-            opacity={0.15}
-          />
-        </group>
-      </Float>
-    </animated.group>
+    <>
+      <animated.group
+        position-y={loadingTransistionValue
+          .to((value) => easings.easeOutExpo(value))
+          .to([0, 1], [3, 0])}
+      >
+        <Float floatIntensity={0.5} speed={0.66} rotationIntensity={0.5}>
+          <group position={[2.66, 1, -5.5]}>
+            <Blob
+              speed={0.2}
+              blobbiness={1}
+              size={1.5}
+              color={"limegreen"}
+              opacity={0.15}
+            />
+          </group>
+        </Float>
+      </animated.group>
+      <animated.group
+        position-y={loadingTransistionValue
+          .to((value) => easings.easeOutCirc(value))
+          .to([0, 1], [3, 0])}
+      >
+        <Float floatIntensity={0.5} speed={0.66} rotationIntensity={0.5}>
+          <group position={[-2.5, 2.5, -6]}>
+            <Blob
+              speed={0.2}
+              blobbiness={1.2}
+              size={3}
+              color={"limegreen"}
+              opacity={0.15}
+            />
+          </group>
+        </Float>
+      </animated.group>
+      <animated.group
+        position-y={loadingTransistionValue
+          .to((value) => easings.easeOutSine(value))
+          .to([0, 1], [-3, 0])}
+      >
+        <Float floatIntensity={0.5} speed={0.5} rotationIntensity={0.5}>
+          <group position={[-0.5, -3.5, -5]}>
+            <Blob
+              speed={0.2}
+              blobbiness={1.1}
+              size={6.5}
+              color={"limegreen"}
+              opacity={0.15}
+            />
+          </group>
+        </Float>
+      </animated.group>
+    </>
   );
 }
 
