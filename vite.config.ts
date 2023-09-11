d205975b4520556965011d2d0ebfe687d0dcd4ca
Bryan <blinz117@gmail.com>
2023-09-10 21:54:02 -0500

    Add path aliases
    Kind of annoying that I have to duplicate the configuration, but it seems like the IDE needs the tsconfig to be happy and the build needs the vite config to be happy, so thus I have both. There might be a better way around this, but I don't have the time to investigate right now

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,12 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@scene": path.resolve(__dirname, "src/scene"),
+      "@analytics": path.resolve(__dirname, "src/analytics"),
     },
   },
 });
