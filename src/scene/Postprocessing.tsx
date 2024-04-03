1ecdc947af7affd6d2851025010d6629be5d2e84
Bryan <blinz117@gmail.com>
2024-04-02 20:59:57 -0500

    Disable postprocessing on lower-tier GPUs

--- src/scene/Postprocessing.tsx
+++ src/scene/Postprocessing.tsx
@@ -1,9 +1,13 @@
 import { AO } from "@/ao";
-import { EffectComposer, N8AO } from "@react-three/postprocessing";
+import { useGpuSettings } from "@/gpuDetection";
+import { EffectComposer } from "@react-three/postprocessing";
 
 export function Postprocessing() {
+  const gpuSettings = useGpuSettings();
+  const showPostprocessing = gpuSettings !== null && gpuSettings.tier > 1;
+
   return (
-    <EffectComposer disableNormalPass>
+    <EffectComposer disableNormalPass enabled={showPostprocessing}>
       {/* <N8AO
         aoRadius={0.5}
         distanceFalloff={0.08}
