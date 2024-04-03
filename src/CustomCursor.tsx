a1df14b7603cc3bb851a0764e7011ddc0e96df29
Bryan <blinz117@gmail.com>
2024-04-03 16:51:58 -0500

    Revert to previous cursor library
    I like the animations on interactions better

--- src/CustomCursor.tsx
+++ src/CustomCursor.tsx
@@ -1,17 +1,21 @@
 import { useAtomValue } from "jotai";
-import { Cursor } from "react-creative-cursor";
+import AnimatedCursor from "react-animated-cursor";
 import { hasDetectedMouse } from "./mousePosition";
-import "react-creative-cursor/dist/styles.css";
 
 export function CustomCursor() {
   const mouseDetected = useAtomValue(hasDetectedMouse);
+
   if (!mouseDetected) return null;
 
   return (
-    <Cursor
-      animationDuration={0.2}
-      cursorSize={20}
-      cursorBackgrounColor="rgba(0, 150, 60, 0.97"
+    <AnimatedCursor
+      innerSize={20}
+      innerScale={2}
+      outerAlpha={0}
+      innerStyle={{
+        backgroundColor: "rgba(0, 150, 60, 0.97)",
+        animation: "squiggly-anim 0.66s linear infinite",
+      }}
     />
   );
 }
