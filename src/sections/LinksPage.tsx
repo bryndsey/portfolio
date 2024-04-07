ec0d51c07506f38ac737eee62a77d2479a21c004
Bryan <blinz117@gmail.com>
2024-04-07 11:13:21 -0500

    Add proper action buttons

--- src/sections/LinksPage.tsx
+++ src/sections/LinksPage.tsx
@@ -160,12 +160,20 @@ function CallToActionContent() {
           Interested in learning more about my work or how we can collaborate?
         </p>
       </div>
-      <a
-        className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-        href="#"
-      >
-        Contact Me
-      </a>
+      <div className="flex flex-row items-baseline gap-2">
+        <a
+          className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          href="mailto:me@bryanlindsey.dev"
+        >
+          Hit me up
+        </a>
+        <a
+          className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          href="https://www.linkedin.com/in/bryan-c-lindsey/"
+        >
+          Connect
+        </a>
+      </div>
     </div>
   );
 }
