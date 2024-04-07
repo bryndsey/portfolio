c01191ace839c692bc11a0f30510e60af9d28ed5
Bryan <blinz117@gmail.com>
2024-04-07 18:33:55 -0500

    Remove old "about me" code

--- src/sections/AboutPage.tsx
+++ src/sections/AboutPage.tsx
@@ -1,9 +1,7 @@
 import { useHtmlPortal } from "@hooks/useHtmlPortal";
-import { useSpringScaleVisibility } from "@hooks/useSpringScaleVisibility";
 import { Html } from "@react-three/drei";
-import { RootState, useFrame } from "@react-three/fiber";
 import { Suspense, useEffect, useRef } from "react";
-import { Group, MathUtils, Vector3 } from "three";
+import { Group } from "three";
 import { PageComponentProps } from "./Pages";
 import { useScrollPages } from "./useScrollPages";
 
@@ -16,136 +14,10 @@ import {
   TypescriptTag,
 } from "./components/ProjectDescription";
 
-interface FloatingTextData {
-  name: string;
-  positionFn: (state: RootState) => number[];
-}
-
-const floatingTextList: FloatingTextData[] = [
-  {
-    name: "React apps",
-    positionFn: (state) => [
-      state.viewport.width * 0.1,
-      state.viewport.height * 0.1,
-      0.4,
-    ],
-  },
-  {
-    name: "Music",
-    positionFn: (state) => [
-      state.viewport.width * -0.25,
-      state.viewport.height * 0.2,
-      -0.1,
-    ],
-  },
-  {
-    name: "Games",
-    positionFn: (state) => [
-      state.viewport.width * 0.3,
-      state.viewport.height * -0.175,
-      -0.1,
-    ],
-  },
-  {
-    name: "3D models",
-    positionFn: (state) => [0, state.viewport.height * -0.25, 0.1],
-  },
-  {
-    name: "Android apps",
-    positionFn: (state) => [
-      state.viewport.width * -0.25,
-      state.viewport.height * -0.1,
-      -0.35,
-    ],
-  },
-];
-
-const groupWorldPosition = new Vector3();
-
-const floatingTextVisibilityThreshold = 0.3;
-
-const FloatingText = (props: { textData: FloatingTextData }) => {
-  const { textData } = props;
-
-  const htmlPortal = useHtmlPortal();
-
-  const groupRef = useRef<Group>(null!);
-  const htmlRef = useRef<HTMLDivElement>(null);
-
-  const { springValue, setVisibility } = useSpringScaleVisibility();
-
-  useFrame((state) => {
-    const position = textData.positionFn(state);
-
-    const floatOffset = [
-      // Offset by amount of a different component for more apparent randomness
-      0.1 *
-        Math.sin(
-          (state.clock.elapsedTime + (position[1] + position[2]) * 80) * 0.3
-        ),
-      0.066 *
-        Math.sin(
-          (state.clock.elapsedTime + (position[2] + position[0]) * 60) * 0.2
-        ),
-      0.025 *
-        Math.sin(
-          (state.clock.elapsedTime + (position[0] + position[1]) * 70) * 0.4
-        ),
-    ];
-
-    groupRef.current.position.set(
-      position[0] + floatOffset[0],
-      position[1] + floatOffset[1],
-      position[2] + floatOffset[2]
-    );
-
-    if (htmlRef.current === null) return;
-    const zPositionOpacity = MathUtils.mapLinear(
-      position[2],
-      -0.5,
-      0.5,
-      0.3,
-      0.8
-    );
-
-    groupWorldPosition.set(0, 0, 0);
-    const worldPosition = groupRef.current.localToWorld(groupWorldPosition);
-
-    const shouldBeVisible =
-      worldPosition.y >
-      -state.viewport.height * floatingTextVisibilityThreshold;
-
-    setVisibility(shouldBeVisible);
-
-    groupRef.current.scale.setScalar(springValue.get());
-
-    htmlRef.current.style.opacity = `${zPositionOpacity}`;
-
-    htmlRef.current.hidden = !groupRef.current.visible;
-  });
-
-  return (
-    <group ref={groupRef}>
-      <Html
-        ref={htmlRef}
-        transform
-        portal={{ current: htmlPortal }}
-        distanceFactor={1}
-        pointerEvents={"none"}
-      >
-        <p className="text-2xl 2xs:text-4xl xs:text-5xl md:text-6xl font-extrabold whitespace-nowrap">
-          {textData.name}
-        </p>
-      </Html>
-    </group>
-  );
-};
-
 export const AboutPage = (props: PageComponentProps) => {
   const htmlPortal = useHtmlPortal();
   const groupRef = useRef<Group>(null);
   const contentRef = useRef<HTMLDivElement>(null);
-  const wordCloudGroupRef = useRef<Group>(null!);
 
   useEffect(() => {
     if (contentRef.current !== null) {
@@ -165,13 +37,6 @@ export const AboutPage = (props: PageComponentProps) => {
 
       groupRef.current.visible = isPageVisible;
 
-      if (wordCloudGroupRef.current.visible != isPageVisible) {
-        wordCloudGroupRef.current.children.forEach(
-          (child) => (child.visible = isPageVisible)
-        );
-        wordCloudGroupRef.current.visible = isPageVisible;
-      }
-
       const yPercent = enterAmount + exitAmount;
 
       const viewportHeight = state.viewport.height;
@@ -182,11 +47,6 @@ export const AboutPage = (props: PageComponentProps) => {
   return (
     <Suspense fallback={null}>
       <group ref={groupRef}>
-        <group ref={wordCloudGroupRef}>
-          {/* {floatingTextList.map((thing) => (
-            <FloatingText key={thing.name} textData={thing} />
-          ))} */}
-        </group>
         <Html
           ref={contentRef}
           fullscreen
@@ -248,4 +108,3 @@ function TechStackItem({ tag }: { tag: Tag }) {
     </div>
   );
 }
-
