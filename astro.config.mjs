b59210e5dea3fdb5aefc17583d46f9631fc69ddd
Bryan <blinz117@gmail.com>
2024-06-06 21:14:30 -0500

    Add suggested changes from integration setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,18 @@ import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sanity()],
+  integrations: [
+    react(),
+    tailwind(),
+    sanity({
+      projectId: "g0lewisa",
+      dataset: "production",
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+      studioBasePath: "/admin",
+    }),
+  ],
   assetsInclude: ["**/*.gltf", "**/*.hdr"],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
